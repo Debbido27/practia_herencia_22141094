a83a3443e1d0239be36100476aef0f3e3fc5beda
Debbido27 <davidmedina272004@gmail.com>
2026-04-28 15:41:04 -0600

    Menu principal, para poder ejecutar diferente clases

--- src/herencia/HERENCIA.java
+++ src/herencia/HERENCIA.java
@@ -1,11 +1,20 @@
 
 package herencia;
 
+import java.util.Scanner;
+
 public class HERENCIA {
 
     
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        Empleado[]empleados=new Empleado[10];
+        int contador=0;
+        int opcion;
+        
+        
+        
     }
     
 }
