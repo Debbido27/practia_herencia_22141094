1041743a68a468928d029590e0cdf39e452daeda
Debbido27 <davidmedina272004@gmail.com>
2026-04-28 15:12:13 -0600

    Creacion de la clase persona

---
+++ src/herencia/Persona.java
@@ -0,0 +1,20 @@
+
+package herencia;
+
+
+public class Persona {
+   String nombre;
+   int edad;
+   String identidad;
+   String direccion;
+   String telefono;
+   
+   public Persona(String nombre, int edad, String identidad, String direccion, String telefono){
+       this.nombre=nombre;
+       this.edad=edad;
+       this.identidad=identidad;
+       this.direccion=direccion;
+       this.telefono=telefono;
+   }
+    
+}
